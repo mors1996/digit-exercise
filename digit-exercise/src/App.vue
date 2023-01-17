bea5ff56d580b9884306d0accebcdff8672de656
mors1996 <moreno.gregori96@gmail.com>
2023-01-17 17:26:06 +0100

    Comment removal and code formatting & some basic comments added

--- digit-exercise/src/App.vue
+++ digit-exercise/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app style="height: 100%">
-    <v-row style="height: 5vh;"></v-row>
-    <v-row style="height: 90vh;" >
+    <v-row style="height: 5vh"></v-row>
+    <v-row style="height: 90vh">
       <v-col cols="4" class="black" style="overflow-y: scroll; height: 100%">
         <v-row v-for="(img, ix) in imageSrcArr" v-bind:key="ix">
           <v-spacer></v-spacer>
@@ -12,14 +12,10 @@
               style="width: 80%"
               class=""
             >
-              Image {{ ix +1 }}
+              Image {{ ix + 1 }}
             </v-btn>
-            <v-btn
-              class="red"
-              v-if="selectedImg == imageSrcArr[ix]"
-              style="width: 80%"
-            >
-              Image {{ ix +1}}
+            <v-btn class="red" v-if="selectedImg == imageSrcArr[ix]" style="width: 80%">
+              Image {{ ix + 1 }}
             </v-btn> </v-col
           ><v-spacer></v-spacer>
         </v-row>
@@ -27,16 +23,20 @@
       <v-col cols="8" class="grey">
         <v-row
           ><v-toolbar
-            ><v-toolbar-title    class="mx-auto clickable" @click="goTo(selectedImg.photographer_url)">{{
-              selectedImg.photographer == null
-                ? "No photographer"
-                : selectedImg.photographer
-            }}</v-toolbar-title></v-toolbar
+            ><v-toolbar-title
+              class="mx-auto clickable"
+              @click="goTo(selectedImg.photographer_url)"
+              >{{
+                selectedImg.photographer == null
+                  ? "No photographer"
+                  : selectedImg.photographer
+              }}</v-toolbar-title
+            ></v-toolbar
           ></v-row
         >
-        <v-row
-          >
-          <v-spacer></v-spacer><v-col cols="8">
+        <v-row>
+          <v-spacer></v-spacer
+          ><v-col cols="8">
             <v-text-field
               color="white"
               append-icon="mdi-magnify"
@@ -48,18 +48,18 @@
               clearable
               @click:append="retrieveImages"
               @keydown.enter="retrieveImages"
-              
             ></v-text-field></v-col
-        ><v-spacer></v-spacer></v-row>
+          ><v-spacer></v-spacer
+        ></v-row>
         <v-row>
           <v-spacer></v-spacer>
           <v-col cols="8"
             ><v-img
-            contain
+              contain
               sizes="200px"
               width="100%"
               height="40vh"
-              :src="selectedImg.src.medium!=null?selectedImg.src.medium:''"
+              :src="selectedImg.src.medium != null ? selectedImg.src.medium : ''"
             ></v-img
           ></v-col>
           <v-spacer></v-spacer>
@@ -67,19 +67,18 @@
         <v-row>
           <v-col cols="4" v-for="ix in 3" v-bind:key="ix"
             ><v-img
-              :class="ix==1?'clickable red-borders':'clickable'"
-              @click="setSelectedImage(parseInt(selectedImg.ix) + ix-1)"
-              
+              :class="ix == 1 ? 'clickable red-borders' : 'clickable'"
+              @click="setSelectedImage(parseInt(selectedImg.ix) + ix - 1)"
               sizes="200px"
               width="100%"
               height="20vh"
-              :src="getSelectedImage(parseInt(selectedImg.ix) + ix-1).src.medium"
+              :src="getSelectedImage(parseInt(selectedImg.ix) + ix - 1).src.medium"
             ></v-img
           ></v-col>
         </v-row>
       </v-col>
     </v-row>
-     <v-row style="height: 5vh;"></v-row>
+    <v-row style="height: 5vh"></v-row>
   </v-app>
 </template>
 
@@ -105,6 +104,9 @@ export default {
   },
 
   methods: {
+    /**
+     * This method retrieves images from backend as links. They will be displayed in image tags
+     */
     retrieveImages() {
       this.imageArr = [];
       axios
@@ -112,11 +114,11 @@ export default {
           headers: { Authorization: this.authToken },
         })
         .then((response) => {
-          console.log(response.data.photos);
+          //console.log(response.data.photos);
           this.imageSrcArr = response.data.photos;
           this.selectedImg = this.imageSrcArr[0];
           this.selectedImg.ix = 0;
-          console.log(this.imageSrcArr);
+          //console.log(this.imageSrcArr);
         })
         .catch((error) => {
           console.log(error);
@@ -124,7 +126,9 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
-
+    /**
+     * This method handles indexes to get the correct image from cached list
+     */
     getSelectedImage(ix) {
       // console.log(ix)
       if (ix >= this.imageSrcArr.length) {
@@ -134,15 +138,19 @@ export default {
         return this.imageSrcArr[delta];
       } else return this.imageSrcArr[ix];
     },
-    goTo(url)
-    {
-      window.open(url, '_blank').focus();
-    }, 
+    // Just a window open with target blank. 
+    goTo(url) {
+      window.open(url, "_blank").focus();
+    },
+
+    /**
+     * This method handles indexes to get the correct image from cached list
+     */
     setSelectedImage(ix) {
       if (ix >= this.imageSrcArr.length) {
         // when index of requested img is above list lenght
         var delta = ix - this.imageSrcArr.length; //
-        console.log(delta);
+        //console.log(delta);
         this.selectedImg = this.imageSrcArr[delta];
         this.selectedImg.ix = delta;
       } else {
@@ -154,15 +162,12 @@ export default {
 };
 </script>
 
-<style scoped> 
-
-.clickable
-{
-    cursor: pointer;
+<style scoped>
+.clickable {
+  cursor: pointer;
 }
-.red-borders
-{
-  border:2px solid; 
+.red-borders {
+  border: 2px solid;
   border-color: red;
 }
 </style>
